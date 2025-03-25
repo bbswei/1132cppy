5a19b0c70c8f96ca760b32def7f3c1b03b03f37f
Akemi-pp <bbswei1989@gmail.com>
2025-03-25 10:15:29 +0800

    readme_add

--- ex2.py
+++ ex2.py
@@ -1,1 +1,15 @@
 ## Week 6 - Ex2
+def game_cheater():
+    digits = int(input()).split("->")[0]
+    hint = [str(input().split("->")[2])]
+    print(digits)
+    print(hint)
+
+
+if __name__ == "__main__":
+    game_cheater()
+
+        
+
+
+
